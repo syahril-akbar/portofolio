fb698ed5021fa59faca6b4144989f34def6da91e
Syahril Akbar <syahrilakbar633@gmail.com>
2026-08-09 17:03:52 +0800

    feat: upgrade to Laravel 13, PHPUnit 13, security fixes
    - Laravel framework 11.x -> 13.24.0 (PHP ^8.4)
    - Filament 5.4.1 -> 5.7.6 (latest)
    - PHPUnit 11 -> 13.3.0
    - laravel/tinker 2.x -> 3.0.2
    - composer audit: 0 vulnerabilities
    - Project model: add tech_stack/is_published casts

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Project extends Model
 {
-    //
+    protected $casts = [
+        'tech_stack' => 'array',
+        'is_published' => 'boolean',
+    ];
 }
